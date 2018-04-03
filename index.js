35b9442a8b463da6fbe722443a261625b2fd2ffa
saibot777 <stefantrajkovic777@gmail.com>
2018-04-03 13:08:29 +0200

    first phase - investigating cluster-mode for perfomance

---
+++ index.js
@@ -0,0 +1,21 @@
+const cluster = require('cluster');
+
+if (cluster.isMaster) {
+    cluster.fork();
+} else {
+    const express = require('express');
+    const app = express();
+
+    const doWork = (duration) => {
+        const start = Date.now();
+        while (Date.now() - start < duration) {}
+    };
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    });
+
+    app.listen(3000);
+}
+
