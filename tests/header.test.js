237613b68b2967b1863d6f54665bd77ef9cf0a99
saibot777 <stefantrajkovic777@gmail.com>
2018-04-10 15:11:30 +0200

    checking for header test impl

--- tests/header.test.js
+++ tests/header.test.js
@@ -6,4 +6,8 @@ test('We can lunch a browser', async () => {
     });
     const page = await browser.newPage();
     await page.goto('localhost:3000');
+
+    const test = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
 });
