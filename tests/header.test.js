74a380aaa64359b7117cb2c178364047b9efc37d
saibot777 <stefantrajkovic777@gmail.com>
2018-04-10 15:06:04 +0200

    implemented puppeteer and chromium, also written the first test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,9 @@
-test('Adds two numbers', () => {
-    const sum = 1 + 2;
+const puppeteer = require('puppeteer');
 
-    expect(sum).toEqual(3);
+test('We can lunch a browser', async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('localhost:3000');
 });
