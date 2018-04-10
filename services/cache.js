ecddbbf558268d388ef500afb2b695229afefe9a
saibot777 <stefantrajkovic777@gmail.com>
2018-04-10 13:44:50 +0200

    refactored caching to use more precise and nested hashing way

--- services/cache.js
+++ services/cache.js
@@ -4,15 +4,26 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function(options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+
+    return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     }));
 
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.get(key).hget(this.hashKey, key);
 
     if (cacheValue) {
         const doc = JSON.parse(cacheValue);
@@ -23,7 +34,7 @@ mongoose.Query.prototype.exec = async function () {
     }
 
     const result = await exec.apply(this, arguments);
-    client.set(key, JSON.stringify(result));
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
     return result;
 };
