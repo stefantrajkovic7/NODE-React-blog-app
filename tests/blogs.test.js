e3201f144d26eb25ca09df80a17df5c4b27d8214
saibot777 <stefantrajkovic777@gmail.com>
2018-04-18 15:22:21 +0200

    testing auth not logged flow part II

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -79,4 +79,19 @@ describe('User is not logged in', async () => {
 
         expect(result).toEqual({ error: 'You must log in!' });
     });
+
+    test('User cannot get a list of posts', async () => {
+        const result = await page.evaluate(
+            () => {
+                return fetch('/api/blogs', {
+                    method: 'GET',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => res.json());
+            });
+
+        expect(result).toEqual({ error: 'You must log in!' });
+    });
 });
