76ec96ae1945f4bd02bb0f7bdd1106a46208c0a5
saibot777 <stefantrajkovic777@gmail.com>
2018-04-18 11:24:56 +0200

    Page Proxy created

---
+++ tests/helpers/page.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+class Page {
+    static async build() {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+
+        const page = await browser.newPage();
+        const customPage = new Page(page);
+
+        return new Proxy(customPage, {
+            get: function(target, property) {
+                return customPage[property] || page[property] || browser[property]
+            }
+        });
+    }
+
+    constructor(page) {
+        this.page = page;
+    }
+}
+
+module.exports = Page;
