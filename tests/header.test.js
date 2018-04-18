b000e6bbb90032ab06db67fad5f136a37dbb7457
saibot777 <stefantrajkovic777@gmail.com>
2018-04-18 12:22:48 +0200

    Proxy impemented for Puppeteer Page/Browser manipulation

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,16 @@
-const puppeteer = require('puppeteer');
 const sessionFactory = require('./factories/sessionFactory');
 const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-        headless: false
-    });
-    page = await browser.newPage();
+    page = await Page.build();
     await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-    // await browser.close();
+    await page.close();
 });
 
 test('We can lunch a browser', async () => {
@@ -30,7 +27,7 @@ test('clicking login starts oauth flow', async () => {
     expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test.only('When signed in, shows logout button', async () => {
+test('When signed in, shows logout button', async () => {
     const user = await userFactory();
     const { session, sig } = sessionFactory(user);
 
