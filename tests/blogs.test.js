0b543d6292027b4648d46a18c722376658ac386c
saibot777 <stefantrajkovic777@gmail.com>
2018-04-18 12:52:30 +0200

    blogs test initiated

---
+++ tests/blogs.test.js
@@ -0,0 +1,11 @@
+const Page = require('./helpers/page');
+let page;
+
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await page.close();
+});
