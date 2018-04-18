23868d5a3550753d37f319362503a78235d3a3ec
saibot777 <stefantrajkovic777@gmail.com>
2018-04-18 10:18:31 +0200

    sessionFactory helper created, refactoring main test suite

--- tests/header.test.js
+++ tests/header.test.js
@@ -31,20 +31,7 @@ test('clicking login starts oauth flow', async () => {
 test.only('When signed in, shows logout button', async () => {
     const id = '5accadf2b526561cb8065404';
 
-    const Buffer = require('safe-buffer').Buffer;
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    };
-    const sessionString = Buffer.from(
-        JSON.stringify(sessionObject))
-            .toString('base64');
 
-    const Keygrip = require('keygrip');
-    const keys = require('../config/keys');
-    const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign('session=' + sessionString);
 
     await page.setCookie({ name: 'session', value: sessionString });
     await page.setCookie({ name: 'session.sig', value: sig });
