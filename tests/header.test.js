c524e81c51878f88d91fe2698840384d2ed0c30a
saibot777 <stefantrajkovic777@gmail.com>
2018-04-18 12:38:40 +0200

    login method refactored

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,7 +1,4 @@
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
 const Page = require('./helpers/page');
-
 let page;
 
 beforeEach(async () => {
@@ -28,14 +25,7 @@ test('clicking login starts oauth flow', async () => {
 });
 
 test('When signed in, shows logout button', async () => {
-    const user = await userFactory();
-    const { session, sig } = sessionFactory(user);
-
-
-    await page.setCookie({ name: 'session', value: session });
-    await page.setCookie({ name: 'session.sig', value: sig });
-    await page.goto('localhost:3000');
-    await page.waitFor('a[href="/auth/logout"]');
+    await page.login();
 
     const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
 
