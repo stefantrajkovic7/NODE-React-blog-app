4554bfe878699a79a2b4265a0b22bb9653ba42b9
saibot777 <stefantrajkovic777@gmail.com>
2018-04-19 10:43:20 +0200

    configuration update

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        '769472234716-3cel4i5dm3i9polf34lo5gtm3v5jaalk.apps.googleusercontent.com',
+    googleClientSecret: 'F5gDTrAbKpcC25FAJ0IF2K_r',
+    mongoURI: '',
+    cookieKey: '123123123',
+    redisUrl: 'redis://127.0.0.1:6379'
+};
--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '769472234716-3cel4i5dm3i9polf34lo5gtm3v5jaalk.apps.googleusercontent.com',
   googleClientSecret: 'F5gDTrAbKpcC25FAJ0IF2K_r',
-  mongoURI: 'mongodb://admin:admin@ds121534.mlab.com:21534/emaily-frenzie',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://127.0.0.1:27017/emaily-frenzie',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
