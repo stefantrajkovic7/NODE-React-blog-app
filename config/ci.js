29b3584e745a431598aa16955d738b303c0af04c
saibot777 <stefantrajkovic777@gmail.com>
2018-04-19 10:54:47 +0200

    mongoURI fix

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID:
         '769472234716-3cel4i5dm3i9polf34lo5gtm3v5jaalk.apps.googleusercontent.com',
     googleClientSecret: 'F5gDTrAbKpcC25FAJ0IF2K_r',
-    mongoURI: '',
+    mongoURI: 'mongodb://127.0.0.1:27017/emaily-frenzie',
     cookieKey: '123123123',
     redisUrl: 'redis://127.0.0.1:6379'
 };
